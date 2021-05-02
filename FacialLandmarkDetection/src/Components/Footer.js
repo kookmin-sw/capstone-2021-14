27cbf54d26ffe139b189705ee40d152f1b1a9b2e
Cha_Y_S <vaite714@gmail.com>
2021-05-02 20:45:27 +0900

    추가: Footer.js / Container 하위 컨테이터 Footer 추가

---
+++ FacialLandmarkDetection/src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Footer = styled.div`
+  display: flex;
+  flex-direction:column;
+  align-items:center;
+  width: 90%;
+  height: 5%;
+  justify-content: center;
+  ${'' /* background-color: #9400D3;
+  margin-top: 24px; */}
+  ${'' /* border-radius: 12px; */}
+  ${'' /* box-shadow: 3px 4px 5px 0 rgba(0, 0, 0, 0.5); */}
+  ${'' /* height: 100%; */}  
+`
+
+export default Footer;
