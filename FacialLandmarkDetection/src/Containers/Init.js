515e5e2f37c8603be2d621072e9d3a28124368bd
Cha_Y_S <vaite714@gmail.com>
2021-05-02 20:50:07 +0900

    추가: Init.js / 초기 페이지 추가

---
+++ FacialLandmarkDetection/src/Containers/Init.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import NextButton from "../Components/NextButton";
+import PrevButton from "../Components/PrevButton";
+import { observer, inject } from "mobx-react";
+
+class InitialContainer extends React.Component {
+	render(){
+		return(
+			<>
+				{/* <Font50>H A I !</Font50> */}
+				<Font30>Hi, AI!</Font30>
+				<Font20>얼굴형 기반 헤어스타일 추천 서비스</Font20>
+				<Font15>서비스를 이용하기 위해 NEXT 버튼을 클릭해주세요.</Font15>
+				{/* <Font15>Robolink AI web app free trial</Font15> */}
+			</>
+		);
+	};
+};
+
+export default InitialContainer;
+
+const Font30 = styled.div`
+  color: #FDFFD5;
+  font-size: 30px;
+  font-weight: normal;
+`;
+
+const Font20 = styled.div`
+  color: #FDFFD5;
+  font-size: 20px;
+  font-weight: normal;
+`;
+
+const Font15 = styled.div`
+  color: #FDFFD5;
+  font-size: 15px;
+  font-weight: normal;
+`;
+
