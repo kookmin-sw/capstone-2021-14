d9e07c29132e4d9b470a020d384beb60c5cef2aa
Cha_Y_S <vaite714@gmail.com>
2021-05-20 23:40:07 +0900

    수정: FaceOutput.js / 결과 이미지 위치 조정

--- FacialLandmarkDetection/src/Containers/FaceOutput.js
+++ FacialLandmarkDetection/src/Containers/FaceOutput.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import "../App.css";
 import styled from "styled-components";
 //import * as tf from "@tensorflow/tfjs";
@@ -26,6 +26,8 @@ let pageIndex;
 const FaceType = ["둥근형", "역삼각형", "계란형", "각진형"];
 
 function FaceOutputContainer() {
+  // const [isDetected, setDetected] = useState(false);
+
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   const imageRef = useRef(null);
@@ -48,7 +50,7 @@ function FaceOutputContainer() {
     intervalId = setInterval(() => {
       console.log("Start detection!");
       detect(net);
-    }, 200); // 200ms
+    }, 1000); // 200ms
   };
 
   // Detect function
@@ -71,17 +73,6 @@ function FaceOutputContainer() {
       predictIrises: false,
     });
 
-    // console.log(face);
-
-    // Get canvas context for drawing
-
-    /*if (ManageFile.pageIndex == 4) {
-      var blob = new Blob([face[0].scaledMesh], {
-        type: "text/plain;charset=utf-8",
-      });
-      saveAs(blob, "data.txt");
-    }*/
-
     const ctx = canvasRef.current.getContext("2d");
     ManageFile.faceType = drawMesh(face, ctx);
 
@@ -90,63 +81,6 @@ function FaceOutputContainer() {
 
   runFacemesh();
 
-  //================================
-
-  // console.log(dfd);
-  // /*let max = 0;
-  //           tf.loadLayersModel('localstorage://my-model').then(function(model) {
-  //               dfd.read_csv('./dataset/norm-all-data.csv').then(function(DATA) {
-  //                   input = DATA.loc({rows: [15]});
-  //                   // 현재는 볼 쪽의 점들도 포함한 dataset으로 추후 이 점들을 제외한 데이터 사용
-  //                   input.drop({ columns: ['260'], axis: 1, inplace: true });
-  //                   const result = model.predict(input.tensor);
-  //                   result.data().then(function(data) {
-  //                       for (let i = 0; i < data.length; i++) {
-  //                           if (data[i] <= 1 && data[i] > max) {
-  //                               max = data[i];
-  //                               max_id = i;
-  //                           }
-  //                       }
-  //                       // 예측값(tensor)에서 최댓값과 인덱스 추출
-  //                       console.log(max);
-  //                       console.log(max_id);
-  //                   });
-  //               });
-  //           });*/
-  // let max = 0;
-  // tf.loadLayersModel("../FaceShape-Model/model-JsonFile/my-model.json").then(
-  //   function (model) {
-  //     dfd
-  //       .read_csv("../FaceShape-Model/dataset/norm-all-data.csv")
-  //       .then(function (DATA) {
-  //         input = DATA.loc({ rows: [14] });
-
-  //         let encoder = new dfd.OneHotEncoder();
-  //         let output = encoder.fit(DATA["260"]);
-  //         console.log(output);
-  //         // 현재는 볼 쪽의 점들도 포함한 dataset으로 추후 이 점들을 제외한 데이터 사용
-  //         input.drop({ columns: ["260"], axis: 1, inplace: true });
-  //         const rvalue = model.predict(input.tensor);
-  //         rvalue.data().then(function (data) {
-  //           for (let i = 0; i < data.length; i++) {
-  //             if (data[i] <= 1 && data[i] > max) {
-  //               max = data[i];
-  //               max_id = i;
-  //             }
-  //           }
-  //           // 예측값(tensor)에서 최댓값과 인덱스 추출
-  //           console.log(max);
-  //           console.log(max_id);
-  //           let result = output.iloc({ columns: [String(max_id)] });
-  //           //얼굴형을 string으로 넘겨준다.
-  //           shape = result.columns[0];
-  //           console.log(shape);
-  //         });
-  //       });
-  //   }
-  // );
-
-  //================================
   return (
     <>
       {/* 당신의 얼굴형은 {ManageFile.faceType} 입니다! */}
@@ -154,7 +88,8 @@ function FaceOutputContainer() {
         당신의 <p fontWeight={"bold"} style={{ color: "blue", display: "inline-block", fontWeight: "bold" }}>얼굴형</p>
         을 확인해보세요.
       </p>
-      <p>인식 중 . . .</p>
+      {/* <p>{!isDetected ? '인식중...' : '인식 완료'}</p> */}
+      <p>인식중. . .</p>
       <ImageContainer>
         <img
           id="test"
@@ -163,10 +98,10 @@ function FaceOutputContainer() {
           style={{
             position: "relative",
             top: 0,
-            left: 0,
+            left: '5%',
+            width: '90%',
+            height: 'auto',
           }}
-          width={"90%"}
-          height={"auto"}
         />
         <canvas
           ref={canvasRef}
@@ -174,153 +109,15 @@ function FaceOutputContainer() {
             position: "absolute",
             top: 0,
             left: '5%',
+            width: '90%',
+            height: 'auto',
           }}
-          width={"90%"}
-          height={"auto"}
         />
       </ImageContainer>
     </>
   );
 }
 
-// import { saveAs } from "FileSaver";
-// import { downcheck } from "Containers/FaceOutput";
-
-// length = 130. dots for detecting face shape
-// 데모 영상 촬영용 좌표
-/*var DOTS = [
-  10,
-  338,
-  297,
-  332,
-  284,
-  251,
-  389,
-  356,
-  454,
-  323,
-  361,
-  288,
-  397,
-  365,
-  379,
-  378,
-  400,
-  377,
-  152,
-  148,
-  176,
-  149,
-  150,
-  136,
-  172,
-  58,
-  132,
-  93,
-  234,
-  127,
-  162,
-  21,
-  54,
-  103,
-  67,
-  109,
-  151,
-  337,
-  299,
-  333,
-  298,
-  301,
-  368,
-  264,
-  447,
-  366,
-  401,
-  435,
-  367,
-  364,
-  394,
-  395,
-  369,
-  396,
-  175,
-  171,
-  140,
-  170,
-  169,
-  135,
-  138,
-  215,
-  177,
-  137,
-  227,
-  34,
-  139,
-  71,
-  68,
-  104,
-  69,
-  108,
-  143,
-  111,
-  117,
-  118,
-  101,
-  36,
-  203,
-  206,
-  216,
-  212,
-  210,
-  214,
-  192,
-  213,
-  147,
-  123,
-  116,
-  50,
-  205,
-  207,
-  187,
-  372,
-  340,
-  346,
-  347,
-  330,
-  266,
-  423,
-  426,
-  436,
-  432,
-  430,
-  434,
-  416,
-  433,
-  376,
-  352,
-  345,
-  280,
-  425,
-  427,
-  411,
-  202,
-  204,
-  194,
-  201,
-  200,
-  421,
-  418,
-  424,
-  422,
-  431,
-  262,
-  428,
-  199,
-  208,
-  32,
-  211,
-];
-*/
 var DOTS = [
   10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 
   378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234, 127, 
@@ -337,6 +134,7 @@ const drawMesh = (predictions, ctx) => {
     console.log("CLEAR!!!!");
     clearInterval(intervalId);
     downcheck = true;
+    // this.setState({isDetected:true});
   }
   if (predictions.length > 0) {
     predictions.forEach((prediction, result) => {
