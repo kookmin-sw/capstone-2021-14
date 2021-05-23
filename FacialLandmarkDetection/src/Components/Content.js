d5617ba6361f493f981623d5e9ecbf3addf061db
Cha_Y_S <vaite714@gmail.com>
2021-05-24 01:30:52 +0900

    수정: Content.js / 반응형 설정

--- FacialLandmarkDetection/src/Components/Content.js
+++ FacialLandmarkDetection/src/Components/Content.js
@@ -8,21 +8,17 @@ const Content = styled.div`
   justify-content: space-around;
   width: 100%;
   margin-top: 12px;
-<<<<<<< HEAD
 	margin-bottom: 18px;
   @media screen and (min-width: 0px) and (max-width: 767.98px) {
-    min-height: 40vh;
+    min-height: 60vh;
   };
   
   @media screen and (min-width: 768px) and (max-width: 1279.98px) {
-    min-height: 50vh;
+    min-height: 55vh;
   };
   @media screen and (min-width: 1280px) {
-    min-height: 60vh;
+    min-height: 50vh;
   };
-=======
-  margin-bottom: 18px;
->>>>>>> d014297d9709b3fa884b975a59b567a09becb6cd
 `;
 
 export default Content;
