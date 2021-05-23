7942a5364002618898b7bdd51f83504132408e23
Cha_Y_S <vaite714@gmail.com>
2021-05-24 01:33:42 +0900

    수정: Front.js / 렌더링 결과 수정

--- FacialLandmarkDetection/src/Containers/Front.js
+++ FacialLandmarkDetection/src/Containers/Front.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import styled from 'styled-components';
+import FaceTypeContainer from './FaceType';
 
 @inject("ManageFile")
 @observer
@@ -9,8 +10,11 @@ class FrontContainer extends Component {
     return(
       <>
         {/* {this.props.ManageFile.faceType != "" && <FaceType>당신의 얼굴형은 <p style={{display: 'inline-block', fontSize: '24px', color: 'blue', fontWeight: 'bold'}}>{this.props.ManageFile.faceType}</p>입니다.</FaceType> } */}
-        {this.props.ManageFile.isFront === true && <IsFront>얼굴이 정면을 향해 있습니다.</IsFront>}
-        {this.props.ManageFile.isFront === false && <IsFront>얼굴을 정면을 향해 맞춰주세요.</IsFront>}
+        {(this.props.ManageFile.isFront === true &&
+        this.props.ManageFile.isCapture === false) && <IsFront>인식중. . .</IsFront>}
+        {(this.props.ManageFile.isFront === false &&
+        this.props.ManageFile.isCapture === false) && <IsFront>얼굴을 정면을 향해 맞춰주세요.</IsFront>}
+        {this.props.ManageFile.isCapture === true && <FaceTypeContainer />}
       </>
     )
   }
